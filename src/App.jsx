f0090a7f5fa5a8920fff341ec821239dec212145
tong <lbuchiyu@gmail.com>
2023-02-07 22:52:43 +0800

    init: init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@alifd/next'
+
+function App () {
+  return (
+    <>
+      <Button>Button</Button>
+      <Button type="primary">Button</Button>
+      <Button text>Button</Button>
+    </>
+  )
+}
+
+export default App
